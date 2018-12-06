cc11b346c32f726eb31a4bc4774876a0632c5f31
clsaa <812022339@qq.com>
2018-12-06 19:49:44 +0800

    finished chapter01

---
+++ effective-practise/src/main/java/com/clsaa/edu/java/effective/chapter01/item05/TestMapSet.java
@@ -0,0 +1,13 @@
+package com.clsaa.edu.java.effective.chapter01.item05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMapSet {
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<String, String>();
+        map.put("123", "!@3");
+        map.entrySet();
+        map.entrySet();
+    }
+}
